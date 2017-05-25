cd5ddadef9ccd2f68f20e6f3436483aa690385e4
shanyongzhou <18069846708@163.com>
2017-05-25 23:50:36 +0800

    地图功能

--- wxapp-hiapp-master/pages/index/index.js
+++ wxapp-hiapp-master/pages/index/index.js
@@ -1,46 +1,101 @@
-let app = getApp()
-let util = require('../../utils/util')
-let ajax = require('../../network/ajax')
-
+//wx-drawer
+const MENU_WIDTH_SCALE = 0.82;
+const FAST_SPEED_SECOND = 300;
+const FAST_SPEED_DISTANCE = 5;
+const FAST_SPEED_EFF_Y = 50;
+var app = getApp()
 Page({
   data: {
-    timeline: [],
-    loading: false  
+    ui: {
+      windowWidth: 0,
+      menuWidth: 0,
+      offsetLeft: 0,
+      tStart: true
+    }
   },
-  onReady() {
-    this.getTimeline()  
+  onLoad() {
+    try {
+      app.getUserInfo(userInfo => {
+        this.setData({
+          userInfo: userInfo,
+        })
+      })
+
+      let res = wx.getSystemInfoSync()
+      this.windowWidth = res.windowWidth;
+      this.data.ui.menuWidth = this.windowWidth * MENU_WIDTH_SCALE;
+      this.data.ui.offsetLeft = 0;
+      this.data.ui.windowWidth = res.windowWidth;
+      this.setData({ ui: this.data.ui })
+    } catch (e) {
+    }
   },
-  getTimeline() {
-    this.setData({
-      loading: true
-    })
-    ajax({
-        url: 'timeline.json',
-        success: res => {
-          let timeline = this.formatTimeline(res.data)
-          this.setData({
-            timeline: timeline
-          })
-        },
-        complete: _ => {
-          this.setData({
-            loading: false
-          })
+  handlerStart(e) {
+    let {clientX, clientY} = e.touches[0];
+    this.tapStartX = clientX;
+    this.tapStartY = clientY;
+    this.tapStartTime = e.timeStamp;
+    this.startX = clientX;
+    this.data.ui.tStart = true;
+    this.setData({ ui: this.data.ui })
+  },
+  handlerMove(e) {
+    let {clientX} = e.touches[0];
+    let {ui} = this.data;
+    let offsetX = this.startX - clientX;
+    this.startX = clientX;
+    ui.offsetLeft -= offsetX;
+    if (ui.offsetLeft <= 0) {
+      ui.offsetLeft = 0;
+    } else if (ui.offsetLeft >= ui.menuWidth) {
+      ui.offsetLeft = ui.menuWidth;
+    }
+    this.setData({ ui: ui })
+  },
+  handlerCancel(e) {
+    // console.log(e);
+  },
+  handlerEnd(e) {
+    this.data.ui.tStart = false;
+    this.setData({ ui: this.data.ui })
+    let {ui} = this.data;
+    let {clientX, clientY} = e.changedTouches[0];
+    let endTime = e.timeStamp;
+    //快速滑动
+    if (endTime - this.tapStartTime <= FAST_SPEED_SECOND) {
+      //向左
+      if (this.tapStartX - clientX > FAST_SPEED_DISTANCE) {
+        ui.offsetLeft = 0;
+      } else if (this.tapStartX - clientX < -FAST_SPEED_DISTANCE && Math.abs(this.tapStartY - clientY) < FAST_SPEED_EFF_Y) {
+        ui.offsetLeft = ui.menuWidth;
+      } else {
+        if (ui.offsetLeft >= ui.menuWidth / 2) {
+          ui.offsetLeft = ui.menuWidth;
+        } else {
+          ui.offsetLeft = 0;
         }
-    })
+      }
+    } else {
+      if (ui.offsetLeft >= ui.menuWidth / 2) {
+        ui.offsetLeft = ui.menuWidth;
+      } else {
+        ui.offsetLeft = 0;
+      }
+    }
+    this.setData({ ui: ui })
   },
-  formatTimeline(items) {
-    items.forEach(item => {
-      item.avatar = util.getAvatarUrl(item.avatar)
-      item.time = util.timeFormat(item.created_at)
-      return item
-    })
-    return items
+  handlerPageTap(e) {
+    let {ui} = this.data;
+    if (ui.offsetLeft != 0) {
+      ui.offsetLeft = 0;
+      this.setData({ ui: ui })
+    }
   },
-  previewImage(event) {
-    wx.previewImage({
-      current: '', 
-      urls: [event.target.dataset.originalPic]
-    })
-  } 
+  handlerAvatarTap(e) {
+    let {ui} = this.data;
+    if (ui.offsetLeft == 0) {
+      ui.offsetLeft = ui.menuWidth;
+      this.setData({ ui: ui })
+    }
+  }
 })
